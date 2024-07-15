b5b325490489eefb8ba2d6f8f9b3c9f29abfb904
ukrWily <ukrwily@gmail.com>
2024-07-15 14:35:55 +0300

    L second

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import css from "rollup-plugin-import-css";
-import { nodeResolve } from "@rollup-plugin-import-css";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 export default {
   input: "src/app.js",
